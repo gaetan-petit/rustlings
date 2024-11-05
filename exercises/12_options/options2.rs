60b92c35917c70e01a29fdf3312b632eb60bacd4
Gaëtan Petit <petit.gaetan@gmail.com>
2024-11-05 22:45:33 +0100

    Second session

--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -9,8 +9,7 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
-        // TODO: Make this an if-let statement whose value is `Some`.
-        word = optional_target {
+        if let Some(word) = optional_target {
             assert_eq!(word, target);
         }
     }
@@ -19,21 +18,18 @@ mod tests {
     fn layered_option() {
         let range = 10;
         let mut optional_integers: Vec<Option<i8>> = vec![None];
-
+    
         for i in 1..=range {
             optional_integers.push(Some(i));
         }
-
+    
         let mut cursor = range;
-
-        // TODO: Make this a while-let statement. Remember that `Vec::pop()`
-        // adds another layer of `Option`. You can do nested pattern matching
-        // in if-let and while-let statements.
-        integer = optional_integers.pop() {
+    
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
-
+    
         assert_eq!(cursor, 0);
     }
 }
